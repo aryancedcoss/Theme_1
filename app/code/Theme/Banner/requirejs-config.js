8674c57969f302ee39043dd32b84a971d4cf2da1
Aryan Cedcoss <aryansrivastava@cedcoss.com>
2020-03-05 10:59:04 +0530

    uploading magento to git -m

---
+++ app/code/Theme/Banner/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {            
+            'owlcarousel': "Theme_Banner/js/owlcarousel"
+        },   
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};
